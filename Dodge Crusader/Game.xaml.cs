fcab8f03afa5c11a6a3edd7d56ba35d4e33f7e4c
MatthewFlenet <matthew.flenet@outlook.com>
2019-12-11 12:06:19 +0100

    Recherche et essais de key-pressed pour pouvoir déplacer le personnage

--- Dodge Crusader/Game.xaml.cs
+++ Dodge Crusader/Game.xaml.cs
@@ -12,25 +12,48 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Dodge_Crusader
 {
     /// <summary>
     /// Logique d'interaction pour Game.xaml
     /// </summary>
+    /// 
+
+    // The ThreadProc method is called when the thread starts.
+    // It loops ten times, writing to the console and yielding 
+    // the rest of its time slice each time, and then ends.
+
+
     public partial class Game : Window
     {
         public Game()
         {
             InitializeComponent();
 
-       
+
+
+
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Game_KeyDown(object sender, System.Windows.Forms.KeyEventArgs key)
         {
-            Hero.Margin 
+            Thickness margin = Hero.Margin; // défini le déplacement du vaisseau
+
+
+            if (margin.Left < 630)
+            {
+                margin.Left = margin.Left + 10;
+                Hero.Margin = margin;
+            }
+            else
+            {
+                margin.Left = margin.Left;
+                Hero.Margin = margin;
+            }
+
         }
+
     }
-}
